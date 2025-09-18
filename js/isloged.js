ad5c0c18da8688a5eb75f20a65b75e06b80412f7
Gianluca-pul <gphgian@gmail.com>
2025-09-18 07:47:03 -0300

    Registro completo + logout

--- js/isloged.js
+++ js/isloged.js
@@ -1,20 +1,6 @@
-document.addEventListener("DOMContentLoaded",function(){
-        const KEY = "userName";
-        const session = localStorage.getItem(KEY);
-    if (!session) {
-    // --- Redirección si no hay usuario ---
-    // Localhost:
-     window.location.href = "http://127.0.0.1:5500/login.html";
-
-    // GitHub Pages:
-    /*window.location.href = "https://donsebamarquez.github.io/EcommerceJap/login.html";*/
-    return;
-  }
-
-  // --- Mostrar correo en la barra ---
-  const slot = document.getElementById("userEmail");
-  if (slot) {
-    slot.textContent = session;
-        }
-
-})
+// Obtiene el nombre del usuario guardado en login_success y lo muestra en la barra
+const slot = document.getElementById("userEmail");
+const session = JSON.parse(localStorage.getItem("login_success"));
+if (slot && session) {
+  slot.textContent = session.name;
+}
--- js/sign-up.js
+++ js/sign-up.js
@@ -1,27 +1,42 @@
 const signupFora = document.querySelector('#signupFora')
+const errorDiv = document.getElementById('signup-error') // selecciona el div que menciona el errror
 
 signupFora.addEventListener('submit', (e) => {
     e.preventDefault()
+    // Limpia cualquier mensaje de error previo
+    errorDiv.textContent = ""
 
-    // Valores de input
-    const Usuario = document.querySelector('#Usuario').value
-    const Email = document.querySelector('#Email').value
-    const Contraseña = document.querySelector('#Contraseña').value
+    const name = document.querySelector('#name').value
+    const email = document.querySelector('#email').value
+    const password = document.querySelector('#password').value
 
-    const userName = JSON.parse(localStorage.getItem('users')) || []
-
-    // Verificar existencia de usuario
-    const isUserRegistered = userName.find(user => user.email === Email)
-    if (isUserRegistered) {
-        alert('El usuario ya está registrado')
+    // Valida que la contraseña tenga al menos 6 caracteres, una mayúscula y una minúscula
+    const passwordRes = /^(?=.*[a-z])(?=.*[A-Z]).{6,}$/;
+    if (!passwordRes.test(password)) {
+        errorDiv.textContent = 'La contraseña debe tener al menos 6 caracteres, una mayúscula, una minúscula';
         return
     }
 
-    // Usuario guardado
-    userName.push({ name: Usuario, email: Email, password: Contraseña })
-    localStorage.setItem('users', JSON.stringify(userName))
+    // utiliza datos existente en localStorage o array vacio si no hay nada y verifica si el mail ya existe
+    const Users = JSON.parse(localStorage.getItem('users')) || []
+    const isUserRegistered = Users.find(user => user.email === email)
 
-    // Usuario redirige al login
-    alert('Registro Exitoso!')
-    window.location.href = 'login.html'
+    // si el mail/usuario ya existe salta cartel y lo redirige al login.html
+    if (isUserRegistered) {
+        alert ('El Usuario ya esta registrado')
+        window.location.href = 'login.html'
+        return
+    }
+
+    // Crea un objeto con los datos del nuevo usuario, con el push lo agrega al array y lo guarada en el localStorage
+    const newUser = { name: name, email: email, password: password }
+    Users.push(newUser)
+    localStorage.setItem('users', JSON.stringify(Users))
+    // guarda la sesion acttiva
+    localStorage.setItem('login_success', JSON.stringify(newUser))
+    errorDiv.textContent = "" // Limpia el error si todo sale bien
+
+    // cartel de Bienvenida y  redirige a index.html
+    alert('Registro exitoso, BIENVENID@')
+    window.location.href = 'index.html'
 })
